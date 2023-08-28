07d6ab7e993ffbd52322935e30aa41cf346d7d4b
Oscar Lima <oscar.lima@dfki.de>
2023-08-28 21:42:07 +0200

    initial code upload

---
+++ src/grasplan/rqt_planning_scene/rqt_planning_scene.py
@@ -0,0 +1,357 @@
+#!/usr/bin/env python3
+
+import os
+import tf
+import math
+import rospy
+import rospkg
+import yaml
+
+from qt_gui.plugin import Plugin
+from python_qt_binding import loadUi
+from python_qt_binding.QtWidgets import QWidget, QFileDialog, QMessageBox
+
+from grasplan.rqt_grasplan.grasps import Grasps
+from grasplan.visualisation.grasp_visualiser import GraspVisualiser
+
+from std_msgs.msg import Int8, String
+from geometry_msgs.msg import Pose, PoseArray, PoseStamped
+
+from grasplan.rqt_planning_scene.vizualize_planning_scene import PlanningSceneVizSettings, PlanningSceneViz
+
+class ErrorDialogPlugin(Plugin):
+    def __init__(self, context):
+        super(ErrorDialogPlugin, self).__init__(context)
+        self.setObjectName('ErrorDialogPlugin')
+
+        self._widget = QtWidgets.QWidget()
+        self._widget.setWindowTitle('Error Dialog Plugin')
+
+        self._show_error_button = QtWidgets.QPushButton('Show Error')
+        self._show_error_button.clicked.connect(self._show_error_dialog)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self._show_error_button)
+        self._widget.setLayout(layout)
+
+        context.add_widget(self._widget)
+
+    def _show_error_dialog(self):
+        error_message = "An error occurred!"
+        QMessageBox.critical(self._widget, "Error", error_message)
+
+class OpenFileDialog(QWidget):
+    '''
+    allow the user to select a different yaml file with a button,
+    this will open a dialog to select and open a yaml file
+    '''
+    def __init__(self, initial_path=None):
+        super().__init__()
+        left, top, width, height = 10, 10, 640, 480
+        self.setGeometry(left, top, width, height)
+        self.initial_path = initial_path
+
+    def openFileNameDialog(self, save_file_name_dialog=False):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        if self.initial_path is None:
+            initial_path = os.environ['HOME']
+        else:
+            initial_path = self.initial_path
+        if save_file_name_dialog:
+            fileName, _ = QFileDialog.getSaveFileName(self, 'Select boxes yaml file',\
+                          initial_path, 'Yaml Files (*.yaml)', options=options)
+        else:
+            fileName, _ = QFileDialog.getOpenFileName(self, 'Select boxes yaml file',\
+                          initial_path, 'Yaml Files (*.yaml)', options=options)
+        if fileName:
+            return fileName
+
+    def saveFileNameDialog(self):
+        return self.openFileNameDialog(save_file_name_dialog=True)
+
+class RqtPlanningScene(Plugin):
+
+    def __init__(self, context):
+        super(RqtPlanningScene, self).__init__(context)
+        rospy.loginfo('Initializing grasplan rqt, have a happy planning scene editing !')
+
+        self.setObjectName('RqtPlanningScene')
+        # Create QWidget
+        self._widget = QWidget()
+        # Get path to UI file (xml description of the gui window created with qtcreator)
+        grasplan_path = rospkg.RosPack().get_path('grasplan')
+        ui_file = os.path.join(grasplan_path, 'config/rqt_planning_scene', 'rqtplanning_scene.ui')
+        # Extend the widget with all attributes and children from UI file
+        loadUi(ui_file, self._widget)
+        self._widget.setObjectName('grasplan - rqt planning scene')
+        # Show _widget.windowTitle on left-top of each plugin (when
+        # it's set in _widget). This is useful when you open multiple
+        # plugins at once. Also if you open multiple instances of your
+        # plugin at once, these lines add number to make it easy to
+        # tell from pane to pane.
+        if context.serial_number() > 1:
+            self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
+
+        # variables
+        #self.write_boxes_to_yaml()
+        #self.update_settings(settings)
+        #self.get_all_boxes_names()
+        self.selected_box = None
+
+        self.settings = PlanningSceneVizSettings()
+        self.settings.yaml_path_to_read = grasplan_path + '/config/examples/planning_scene.yaml'
+        self.settings.yaml_path_to_write = grasplan_path + '/config/examples/auto_generated_planning_scene.yaml'
+        self.settings.publication_type = 'single'
+        self.psv = PlanningSceneViz(self.settings) # TODO: add use case: create planning scene from scratch
+        self.psv.publish_boxes()
+
+        self.all_boxes_names = self.psv.get_all_boxes_names()
+
+        self._widget.comboExistingBoxes.addItems([''] + self.all_boxes_names)
+
+        # publications
+
+        # parameters
+
+        # make a connection between the qt objects and this class methods
+
+        # buttons
+        self._widget.cmdAddNew.clicked.connect(self.handle_cmdAddNew)
+        self._widget.cmdLoadYaml.clicked.connect(self.handle_cmdLoadYaml)
+        self._widget.cmdSaveYaml.clicked.connect(self.handle_cmdSaveYaml)
+        self._widget.cmdReset.clicked.connect(self.handle_cmdReset)
+
+        # slide change event
+        self._widget.slideRoll.valueChanged.connect(self.slideRoll_value_changed)
+        self._widget.slidePitch.valueChanged.connect(self.slidePitch_value_changed)
+        self._widget.slideYaw.valueChanged.connect(self.slideYaw_value_changed)
+        self._widget.slideX.valueChanged.connect(self.slideX_value_changed)
+        self._widget.slideY.valueChanged.connect(self.slideY_value_changed)
+        self._widget.slideZ.valueChanged.connect(self.slideZ_value_changed)
+
+        # combo box changed selection
+        self._widget.comboExistingBoxes.currentIndexChanged.connect(self.comboExistingBoxes_changed)
+
+        # chk hide changes
+        self.hide_chks = [self._widget.chkHide1,
+                            self._widget.chkHide2,
+                            self._widget.chkHide3,
+                            self._widget.chkHide4,
+                            self._widget.chkHide5,
+                            self._widget.chkHide6,
+                            self._widget.chkHide7,
+                            self._widget.chkHide8,
+                            self._widget.chkHide9,
+                            self._widget.chkHide10]
+        for hide_chk in self.hide_chks:
+            hide_chk.stateChanged.connect(self.chkHide_changed)
+
+        # viz widgets
+        self.viz_widgets = [self._widget.txtViz1,
+                            self._widget.txtViz2,
+                            self._widget.txtViz3,
+                            self._widget.txtViz4,
+                            self._widget.txtViz5,
+                            self._widget.txtViz6,
+                            self._widget.txtViz7,
+                            self._widget.txtViz8,
+                            self._widget.txtViz9,
+                            self._widget.txtViz10]
+
+        # populate visibility panel box names
+        for i, box_name in enumerate(self.all_boxes_names):
+            if i < len(self.viz_widgets):
+                self.viz_widgets[i].setPlainText(box_name)
+            else:
+                rospy.logwarn('visibility panel capacity exceeded, ignoring some boxes')
+
+        self.psv.publish_boxes()
+        context.add_widget(self._widget)
+        rospy.loginfo('planning scene rqt initialization complete')
+        # end of constructor
+
+    # ::::::::::::::  class methods
+
+    def hide(self, scene_name):
+        self.psv.settings.ignore_set.add(scene_name)
+
+    def unhide(self, scene_name):
+        if scene_name in self.psv.settings.ignore_set:
+            self.psv.settings.ignore_set.remove(scene_name)
+
+    def chkHide_changed(self):
+        for i, hide_chk in enumerate(self.hide_chks):
+            scene_name = self.viz_widgets[i].toPlainText()
+            print(f'self.psv.settings.ignore_set = {self.psv.settings.ignore_set}')
+            if hide_chk.isChecked():
+                self.hide(scene_name)
+            else:
+                self.unhide(scene_name)
+        self.psv.publish_boxes()
+
+    def handle_cmdReset(self):
+        self.psv.reset_scene_name(self._widget.comboExistingBoxes.currentText())
+        # self._widget.comboExistingBoxes.setCurrentIndex(0)
+
+    def update_slide_values(self, scene_name):
+        box = self.psv.get_box_values(scene_name)
+        print(box)
+        value_x, min_x, max_x  = self.get_slide_value_and_limits('x')
+        value_y, min_y, max_y  = self.get_slide_value_and_limits('y')
+        value_z, min_z, max_z  = self.get_slide_value_and_limits('z')
+
+        box_position_x = box['box_position_x']
+        box_position_y = box['box_position_y']
+        box_position_z = box['box_position_z']
+
+        # make sure box values are within range
+        #continue_ = False
+        #if box_position_x > min_x:
+            #if box_position_x < max_x:
+                #if box_position_y < min_y:
+                    #if box_position_y < max_y:
+                        #if box_position_z < min_z:
+                            #if box_position_z < max_z:
+                                #continue_ = True
+        #if not continue_:
+            #rospy.logwarn('box values are outside limits, will not update slide values')
+            #return
+
+        # diff   -> 100%   # diff = max_scroll_limit - min_scroll_limit
+        # value  -> slide_value?  # value = box_position_x
+        slide_value_x = (box_position_x - min_x) * 100.0 / (max_x - min_x)
+        slide_value_y = (box_position_y - min_y) * 100.0 / (max_y - min_y)
+        slide_value_z = (box_position_z - min_z) * 100.0 / (max_z - min_z)
+
+        self._widget.slideX.setValue(slide_value_x)
+        self._widget.slideY.setValue(slide_value_y)
+        self._widget.slideZ.setValue(slide_value_z)
+
+    def select_box(self, scene_name):
+        self.selected_box = scene_name
+        self.psv.settings.colors = {}
+        self.psv.settings.colors[scene_name] = 'orange'
+        self.psv.publish_boxes()
+        self.update_slide_values(scene_name)
+
+    def comboExistingBoxes_changed(self):
+        combo_text = self._widget.comboExistingBoxes.currentText()
+        if combo_text != '':
+            rospy.loginfo(f'selecting box: {combo_text}')
+            self.select_box(combo_text)
+
+    def handle_cmdAddNew(self):
+        print(self._widget.txtRefFrame.toPlainText())
+
+    def handle_cmdLoadYaml(self):
+        print('not implemented')
+
+    def handle_cmdSaveYaml(self):
+        print('not implemented')
+
+    def slideRoll_value_changed(self):
+        if self.selected_box:
+            print(self._widget.slideRoll.value())
+        else:
+            self.error()
+
+    def slidePitch_value_changed(self):
+        if self.selected_box:
+            print(self._widget.slidePitch.value())
+        else:
+            self.error()
+
+    def slideYaw_value_changed(self):
+        if self.selected_box:
+            print(self._widget.slideYaw.value())
+        else:
+            self.error()
+
+    def error(self, error_msg='No box selected!'):
+        QMessageBox.critical(self._widget, "Error", error_msg)
+
+    def compute_value(self, axis):
+        slide_value, min_scroll_limit, max_scroll_limit = self.get_slide_value_and_limits(axis)
+        if self.selected_box:
+            rospy.logdebug(f'slide_value : {slide_value}')
+            # diff   -> 100%   # diff = max_scroll_limit - min_scroll_limit
+            # value? -> slide_value
+            value = min_scroll_limit + slide_value * (max_scroll_limit - min_scroll_limit) / 100.0
+            rospy.logdebug(f'box position = {value}')
+            return value
+        else:
+            rospy.logwarn('no box selected, doing nothing...')
+            self.error()
+
+    def get_slide_value_and_limits(self, slide):
+        '''
+        slide can take values of 'x', 'y', or 'z'
+        '''
+        if slide == 'x':
+            return float(self._widget.slideX.value()),\
+                    float(self._widget.txtScrollMinX.toPlainText()),\
+                    float(self._widget.txtScrollMaxX.toPlainText())
+        elif slide == 'y':
+            return float(self._widget.slideY.value()),\
+                    float(self._widget.txtScrollMinY.toPlainText()),\
+                    float(self._widget.txtScrollMaxY.toPlainText())
+        elif slide == 'z':
+            return float(self._widget.slideZ.value()),\
+                    float(self._widget.txtScrollMinZ.toPlainText()),\
+                    float(self._widget.txtScrollMaxZ.toPlainText())
+
+    def slideX_value_changed(self):
+        value = self.compute_value('x')
+        if value:
+            self.psv.modify_box(self.selected_box, modify_box_position_x=True, box_position_x=value)
+
+    def slideY_value_changed(self):
+        value = self.compute_value('y')
+        if value:
+            self.psv.modify_box(self.selected_box, modify_box_position_y=True, box_position_y=value)
+
+    def slideZ_value_changed(self):
+        value = self.compute_value('z')
+        if value:
+            self.psv.modify_box(self.selected_box, modify_box_position_z=True, box_position_z=value)
+
+
+#txtSceneName
+#comboRefFrame
+#comboExistingBoxes
+
+#buttons
+
+#cmdAddNew
+#cmdSelectExisting
+#cmdLoadYaml
+#cmdSaveYaml
+
+#slides
+
+#slideRoll
+#slidePitch
+#slideYaw
+
+#slideX
+#slideY
+#slideZ
+
+# transform
+
+#txtTFX
+#txtTFY
+#txtTFZ
+#txtTFQx
+#txtTFQy
+#txtTFQz
+#txtTFQw
+
+#txtScrollMinX
+#txtScrollMinY
+#txtScrollMinZ
+
+#txtScrollMaxX
+#txtScrollMaxY
+#txtScrollMaxZ
---
+++ src/grasplan/rqt_planning_scene/vizualize_planning_scene.py
@@ -0,0 +1,292 @@
+#!/usr/bin/env python3
+
+'''
+reads from yaml a list of boxes and publishes them as a msgs/MarkerArray to the grasplan_planning_scene topic
+'''
+
+import rospy
+import rospkg
+
+import os
+import sys
+import yaml
+import copy
+
+from visualization_msgs.msg import Marker, MarkerArray
+
+class PlanningSceneVizSettings:
+    # default settings
+    text_on = True
+    ignore_set = set()
+    ignore_all_but = []
+    publication_type = 'latch' # admissible values: 'latch', 'single'
+    topic = 'grasplan_planning_scene'
+    colors = {}
+    marker_ns = topic
+    transparency = 1.0
+    yaml_path_to_read = ''
+    yaml_path_to_write = ''
+
+class PlanningSceneViz:
+    def __init__(self, settings, load_boxes_from_yaml=True):
+        if not self.validate_settings(settings):
+            rospy.logerr('invalid settings, terminating node')
+            rospy.signal_shutdown('invalid settings, shutting down...')
+        self.settings = settings
+        self.box_list_dictionary = {}
+
+        # remove
+        #self.settings.ignore_set = {'insole_rejection_box'}
+        #self.settings.ignore_all_but = []
+        #self.settings.publication_type = 'single'
+        ##self.settings.publication_type = 'latch'
+        ##self.settings.publication_type = 10
+        #self.settings.colors = {'camera':'purple'}
+        #self.settings.transparency = 0.8
+        ##self.settings.yaml_path_to_read = '/home/oscar/ros_ws/new_april_ws/src/april_manipulation/grasplan/config/output_planning_scene.yaml'
+
+        self.marker_id_count = 0
+        self.all_boxes_names = []
+
+        if load_boxes_from_yaml:
+            self.load_boxes_from_yaml(self.settings.yaml_path_to_read)
+
+        if self.settings.publication_type == 'latch':
+            self.marker_array_pub = rospy.Publisher(self.settings.topic, MarkerArray, queue_size=1, latch=True)
+        elif self.settings.publication_type == 'single':
+            self.marker_array_pub = rospy.Publisher(self.settings.topic, MarkerArray, queue_size=1)
+
+        # delete old data if any
+        self.delete_all_markers(sleep=True)
+
+        rospy.loginfo('vizualize planning scene node initialized')
+
+    def validate_settings(self, settings):
+        admisible_pub_type = ['latch', 'single']
+        if settings.publication_type not in admisible_pub_type:
+            rospy.logerr(f'publication_type can only have one of these values: {admisible_pub_type}')
+            return False
+        if settings.transparency > 1.0 or settings.transparency < 0.0:
+            rospy.logerr('transparency setting has to be between 0.0 and 1.0')
+            return False
+        return True
+
+    def update_settings(self, settings):
+        self.settings = settings
+
+    def get_box_values(self, scene_name):
+        for box in self.box_list_dictionary:
+            if box['scene_name'] == scene_name:
+                return box
+
+    def reset_scene_name(self, scene_name):
+        for box in self.box_list_dictionary:
+            if box['scene_name'] == scene_name:
+                for box_bkp in self.box_list_dictionary_bkp:
+                    if box_bkp['scene_name'] == scene_name:
+                        self.modify_box(scene_name, modify_box_position_x=True, box_position_x=box_bkp['box_position_x'], modify_box_position_y=True, box_position_y=box_bkp['box_position_y'],
+                            modify_box_position_z=True, box_position_z=box_bkp['box_position_z'],
+                            modify_box_orientation_x=True, box_orientation_x=box_bkp['box_orientation_x'],
+                            modify_box_orientation_y=True, box_orientation_y=box_bkp['box_orientation_y'],
+                            modify_box_orientation_z=True, box_orientation_z=box_bkp['box_orientation_z'],
+                            modify_box_orientation_w=True, box_orientation_w=box_bkp['box_orientation_w'],
+                            modify_box_x_dimension=True, box_x_dimension=box_bkp['box_x_dimension'],
+                            modify_box_y_dimension=True, box_y_dimension=box_bkp['box_y_dimension'],
+                            modify_box_z_dimension=True, box_z_dimension=box_bkp['box_z_dimension'])
+                        return
+        rospy.logerr(f'could not found scene name: {scene_name}')
+
+    def modify_box(self, scene_name,
+                   modify_box_position_x=False, box_position_x=0.0,
+                   modify_box_position_y=False, box_position_y=0.0,
+                   modify_box_position_z=False, box_position_z=0.0,
+                   modify_box_orientation_x=False, box_orientation_x=0.0,
+                   modify_box_orientation_y=False, box_orientation_y=0.0,
+                   modify_box_orientation_z=False, box_orientation_z=0.0,
+                   modify_box_orientation_w=False, box_orientation_w=1.0,
+                   modify_box_x_dimension=False, box_x_dimension=1.0,
+                   modify_box_y_dimension=False, box_y_dimension=1.0,
+                   modify_box_z_dimension=False, box_z_dimension=1.0):
+        found = False
+        for box in self.box_list_dictionary:
+            if box['scene_name'] == scene_name:
+                found = True
+                if modify_box_position_x:
+                    box['box_position_x'] = box_position_x
+                if modify_box_position_y:
+                    box['box_position_y'] = box_position_y
+                if modify_box_position_z:
+                    box['box_position_z'] = box_position_z
+                if modify_box_orientation_x:
+                    box['box_orientation_x'] = box_orientation_x
+                if modify_box_orientation_y:
+                    box['box_orientation_y'] = box_orientation_y
+                if modify_box_orientation_z:
+                    box['box_orientation_z'] = box_orientation_z
+                if modify_box_orientation_w:
+                    box['box_orientation_w'] = box_orientation_w
+                if modify_box_x_dimension:
+                    box['box_x_dimension'] = box_x_dimension
+                if modify_box_y_dimension:
+                    box['box_y_dimension'] = box_y_dimension
+                if modify_box_z_dimension:
+                    box['box_z_dimension'] = box_z_dimension
+        self.publish_boxes()
+        if not found:
+            rospy.logerr(f'cannot modify box, scene_name : {scene_name} not found')
+
+    def load_boxes_from_yaml(self, yaml_path):
+        if not os.path.exists(yaml_path):
+            rospy.logerr('yaml path does not exist')
+            return
+        with open(yaml_path, 'r') as file:
+            yaml_content = file.read()
+        self.box_list_dictionary = yaml.safe_load(yaml_content)['planning_scene_boxes']
+        self.box_list_dictionary_bkp = copy.deepcopy(self.box_list_dictionary)
+        # construct a list of all boxes
+        self.all_boxes_names = []
+        for box in self.box_list_dictionary:
+            self.all_boxes_names.append(box['scene_name'])
+        rospy.loginfo(f'loaded {len(self.all_boxes_names)} planning scene boxes')
+
+    def get_all_boxes_names(self):
+        return self.all_boxes_names
+
+    def write_boxes_to_yaml(self, yaml_path_to_write, from_settings=False):
+        if from_settings:
+            yaml_path_to_write = self.settings.yaml_path_to_write
+        with open(yaml_path_to_write, 'w') as yaml_file:
+            master_dictionary = {'planning_scene_boxes': self.box_list_dictionary}
+            yaml.dump(master_dictionary, yaml_file, default_flow_style=False)
+
+    def symbolic_to_rgb_color(self, symbolic_color):
+        if symbolic_color == 'green':
+            return [0.0, 1.0, 0.0]
+        if symbolic_color == 'blue':
+            return [0.0, 0.0, 1.0]
+        if symbolic_color == 'red':
+            return [1.0, 0.0, 0.0]
+        if symbolic_color == 'purple':
+            return [1.0, 0.0, 1.0]
+        if symbolic_color == 'orange':
+            return [1.0, 0.65, 0.0]
+        rospy.logwarn(f'color {symbolic_color} not supported, will use default green color')
+        rospy.logwarn(f'supported colors are: green, blue, red, purple, orange')
+        return [0.0, 1.0, 0.0]
+
+    def make_text_marker(self, marker, text):
+        '''
+        edits an existing marker overwritting the text field
+        '''
+        marker.type = Marker.TEXT_VIEW_FACING
+        marker.id = self.marker_id_count
+        self.marker_id_count += 1
+        marker.text = text
+        marker.pose.position.z += marker.scale.z / 2.0 + 0.1
+        marker.scale.z = 0.2 # text size
+        marker.color.r = 1.0
+        marker.color.g = 1.0
+        marker.color.b = 1.0
+        return marker
+
+    def make_marker(self, scene_name, frame_id, origin_x, origin_y, origin_z,
+                    orientation_x, orientation_y, orientation_z, orientation_w,
+                    scale_x, scale_y, scale_z):
+        rgb_color_list = self.symbolic_to_rgb_color('green') # default
+        if scene_name in self.settings.colors:
+            rgb_color_list = self.symbolic_to_rgb_color(self.settings.colors[scene_name])
+        marker = Marker()
+        marker.header.frame_id = frame_id
+        marker.ns = self.settings.marker_ns
+        marker.id = self.marker_id_count
+        self.marker_id_count += 1
+        marker.type = Marker.CUBE
+        marker.action = Marker.ADD
+        marker.pose.position.x = origin_x
+        marker.pose.position.y = origin_y
+        marker.pose.position.z = origin_z
+        marker.pose.orientation.x = orientation_x
+        marker.pose.orientation.y = orientation_y
+        marker.pose.orientation.z = orientation_z
+        marker.pose.orientation.w = orientation_w
+        marker.scale.x = scale_x
+        marker.scale.y = scale_y
+        marker.scale.z = scale_z
+        marker.color.a = self.settings.transparency
+        marker.color.r = rgb_color_list[0]
+        marker.color.g = rgb_color_list[1]
+        marker.color.b = rgb_color_list[2]
+        return marker
+
+    def delete_all_markers(self, sleep=False):
+        self.marker_id_count = 0
+        marker_array_msg = MarkerArray()
+        marker = Marker()
+        marker.id = 0
+        marker.ns = self.settings.marker_ns
+        marker.action = Marker.DELETEALL
+        marker_array_msg.markers.append(marker)
+        self.marker_array_pub.publish(marker_array_msg)
+        if sleep:
+            rospy.sleep(0.03)
+
+    def publish_boxes(self):
+        self.delete_all_markers()
+        marker_array_msg = MarkerArray()
+
+        ignore_set = self.settings.ignore_set
+        if self.settings.ignore_all_but != []:
+            # automatically build ignore set
+            # ignore_set = all_boxes - self.settings.ignore_all_but
+            ignore_set = set([item for item in self.all_boxes_names if item not in self.settings.ignore_all_but])
+
+        for box in self.box_list_dictionary:
+            if box['scene_name'] in ignore_set:
+                continue
+            marker = self.make_marker(box['scene_name'],
+                                box['frame_id'],
+                                box['box_position_x'],
+                                box['box_position_y'],
+                                box['box_position_z'], 
+                                box['box_orientation_x'],
+                                box['box_orientation_y'],
+                                box['box_orientation_z'],
+                                box['box_orientation_w'],
+                                box['box_x_dimension'],
+                                box['box_y_dimension'],
+                                box['box_z_dimension'])
+            marker_array_msg.markers.append(marker)
+            if self.settings.text_on:
+                text_marker = self.make_text_marker(copy.deepcopy(marker), box['scene_name'])
+                marker_array_msg.markers.append(text_marker)
+
+        if self.settings.publication_type == 'latch':
+            self.marker_array_pub.publish(marker_array_msg)
+            rospy.spin()
+        elif self.settings.publication_type == 'single':
+            self.marker_array_pub.publish(marker_array_msg)
+
+if __name__ == '__main__':
+    rospy.init_node('planning_scene_publisher', anonymous=False)
+    if len(sys.argv) > 1:
+        yaml_path = sys.argv[1]
+    else:
+        package_path = rospkg.RosPack().get_path('april_pick_place_object')
+        yaml_path = package_path + '/config/grasplan/planning_scene.yaml'
+    if os.path.exists(yaml_path):
+        # yaml file path exists
+        settings = PlanningSceneVizSettings()
+        # a list of planning scene boxes to ignore (not draw)
+        settings.ignore_set = set(rospy.get_param('~ignore_set', []))
+        # has precedence over ignore_set, nothing else but boxes on this list will be draw
+        settings.ignore_all_but = rospy.get_param('~ignore_all_but', [])
+        settings.publication_type = rospy.get_param('~publication_type', 'single')
+        settings.colors = rospy.get_param('~colors', {})
+        settings.transparency = rospy.get_param('~transparency', 0.8)
+        settings.yaml_path_to_read = yaml_path
+        settings.yaml_path_to_write = rospkg.RosPack().get_path('grasplan') + '/config/output_planning_scene.yaml'
+        psv = PlanningSceneViz(settings)
+        psv.publish_boxes()
+        rospy.sleep(0.2)
+    else:
+        print("yaml file does not exist:", yaml_path)
