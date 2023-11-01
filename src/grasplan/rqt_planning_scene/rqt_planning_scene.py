2016806bba1cac67297e7b641a967b141f4203b0
Oscar Lima <oscar.lima@dfki.de>
2023-11-01 11:33:14 +0100

    rqt planning scene: remove unnecesary setting

--- src/grasplan/rqt_planning_scene/rqt_planning_scene.py
+++ src/grasplan/rqt_planning_scene/rqt_planning_scene.py
@@ -99,7 +99,6 @@ class RqtPlanningScene(Plugin):
         self.settings = PlanningSceneVizSettings()
         self.settings.yaml_path_to_read = rospy.get_param('~yaml_path_to_read', grasplan_path + '/config/examples/planning_scene.yaml')
         self.settings.yaml_path_to_write = rospy.get_param('~yaml_path_to_write', grasplan_path + '/config/examples/auto_generated_planning_scene.yaml')
-        self.settings.publication_type = 'single'
         self.psv = PlanningSceneViz(self.settings) # TODO: add use case: create planning scene from scratch
         self.psv.publish_boxes()
 
